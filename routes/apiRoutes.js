9ddd8a1eeb71eae7e7ee4d8663a4eada99a946f5
skinsler <simonkinsler@gmail.com>
2019-07-02 23:27:14 -0500

    completed intial dummy routes, updated package.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,5 +7,52 @@ router.route('/sightings')
         res.json(seeds);
     })
 
+router.route('/user/:id/sightings')
+    .get((req,res,err) => {
+        res.json(seeds);
+    })
+    .post((req,res,err) => {
+        res.json("post sighting");
+    })
+
+router.route('/user/:id/sightings/:id')
+    .get((req,res,err) => {
+        res.json(seeds[0]);
+    })
+    .put((req,res,err) => {
+        res.json("edit sighting");
+    })
+    .delete((req,res,err) => {
+        res.json("delete sighting");
+    })
+
+router.route('/search')
+    .get((req,res,err) => {
+        res.json(seeds);
+    })
+
+router.route('/sightings/:id/comments')
+    .get((req,res,err) => {
+        res.json("get comments");
+    })
+    .post((req,res,err) => {
+        res.json("post");
+    })
+
+router.route('/sightings/:id/comments/:id')
+    .get((req,res,err) => {
+        res.json("comment");
+    })
+    .put((req,res,err) => {
+        res.json("edit comment");
+    })
+
+router.route('/user')
+    .get((req,res,err) => {
+        res.json("get user");
+    })
+    .post((req,res,err) => {
+        res.json("post user");
+    })
 
 module.exports = router;
